113f940bd1c8da9753b0b74e697f8bd68ac981d9
”Valeriy <valerkaf@mail.ru>
2025-01-30 12:02:20 +0400

    Final project with Javadoc

--- src/homeWork1/list/CustomList.java
+++ src/homeWork1/list/CustomList.java
@@ -5,6 +5,9 @@ import java.util.Comparator;
 public interface CustomList<T> {
     void add(T element);
     void add(int index, T element);
+    /**
+     * Возвращает элемент, находящийся в указанной позиции в этом списке.
+     */
     T get(int index);
     void remove(int index);
     void clear();
